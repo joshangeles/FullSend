b7bc8a9e4f662b19ce00fd03e9a93f918a19b19b
Joshua Angeles <mintoesverm@yahoo.com>
2023-01-12 22:02:26 +0000

    Event Information section now populated with retrieved data stored in . Includes clickable link to ticket listing. #1

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 var $searchForm = document.querySelector('#searchForm');
 var $searchInput = $searchForm.querySelector('#searchInput');
-
+var $previewImage = document.querySelector('#previewImage');
+var $eventInfo = document.querySelectorAll('.data-placeholder');
 function searchHandler(event, name) {
   event.preventDefault();
   name = $searchInput.value;
@@ -16,6 +17,35 @@ function searchHandler(event, name) {
     data.currentEvent.address = xhr.response._embedded.events[0]._embedded.venues[0].address.line1 + ', ' + xhr.response._embedded.events[0]._embedded.venues[0].city.name + ', ' + xhr.response._embedded.events[0]._embedded.venues[0].state.stateCode;
     data.currentEvent.imageURL = xhr.response._embedded.events[0].images[0].url;
     data.currentEvent.artist = xhr.response._embedded.events[0]._embedded.attractions[0].name;
+    $previewImage.setAttribute('src', data.currentEvent.imageURL);
+    function setVisible(index) {
+      if ($eventInfo[index].tagName === 'P') {
+        $eventInfo[index].className = 'data-placeholder text-black text-end visible';
+      }
+      if ($eventInfo[index].tagName === 'A') {
+        $eventInfo[index].className = 'data-placeholder text-decoration-none float-end visible';
+      }
+    }
+    for (var i = 0; i < $eventInfo.length; i++) {
+      if ($eventInfo[i].tagName === 'P') {
+        if ($eventInfo[i].textContent === 'artistPlaceholder') {
+          $eventInfo[i].textContent = data.currentEvent.artist;
+        }
+        if ($eventInfo[i].textContent === 'venuePlaceholder') {
+          $eventInfo[i].textContent = data.currentEvent.venue;
+        }
+        if ($eventInfo[i].textContent === 'addressPlaceholder') {
+          $eventInfo[i].textContent = data.currentEvent.address;
+        }
+        if ($eventInfo[i].textContent === 'datePlaceholder') {
+          $eventInfo[i].textContent = data.currentEvent.date;
+        }
+      } else if ($eventInfo[i].tagName === 'A') {
+        $eventInfo[i].textContent = data.currentEvent.name;
+        $eventInfo[i].setAttribute('href', data.currentEvent.eventURL);
+      }
+      setVisible(i);
+    }
   });
 }
 
