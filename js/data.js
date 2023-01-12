7cf0ad8e3d2e7bdc75318c1a04984db7471cce6d
Joshua Angeles <mintoesverm@yahoo.com>
2023-01-12 17:50:36 +0000

    Created data model with all necessary properties for data storage. #1

--- js/data.js
+++ js/data.js
@@ -3,5 +3,13 @@
 var data = {
   view: 'form',
   events: [],
+  currentEvent: {
+    imageURL: null,
+    artist: null,
+    name: null,
+    venue: null,
+    address: null,
+    date: null
+  },
   nextEventId: 1
 };
