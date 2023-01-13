3d22b658742d9c49bf5a4703a8cb1b2c7b1b3d6d
Joshua Angeles <mintoesverm@yahoo.com>
2023-01-13 00:39:06 +0000

    Fixed bug where data would not update on event information. #1

--- js/main.js
+++ js/main.js
@@ -25,7 +25,6 @@ function searchHandler(event, name) {
   var xhr = new XMLHttpRequest();
   xhr.open('GET', 'https://app.ticketmaster.com/discovery/v2/events.json?size=1&apikey=r3mO1M5MAEVbprAB3NakjYfqW8q0obAh&sort=date,asc&keyword=' + name);
   xhr.responseType = 'json';
-  xhr.send();
   xhr.addEventListener('load', function () {
     data.currentEvent.name = xhr.response._embedded.events[0].name;
     data.currentEvent.eventURL = xhr.response._embedded.events[0].url;
@@ -34,34 +33,48 @@ function searchHandler(event, name) {
     data.currentEvent.address = xhr.response._embedded.events[0]._embedded.venues[0].address.line1 + ', ' + xhr.response._embedded.events[0]._embedded.venues[0].city.name + ', ' + xhr.response._embedded.events[0]._embedded.venues[0].state.stateCode;
     data.currentEvent.imageURL = xhr.response._embedded.events[0].images[0].url;
     data.currentEvent.artist = xhr.response._embedded.events[0]._embedded.attractions[0].name;
-    $previewImage.setAttribute('src', data.currentEvent.imageURL);
 
+    $previewImage.setAttribute('src', data.currentEvent.imageURL);
     for (var i = 0; i < $eventInfo.length; i++) {
       if ($eventInfo[i].tagName === 'P') {
-        if ($eventInfo[i].textContent === 'artistPlaceholder') {
+        if ($eventInfo[i].getAttribute('id') === 'artist') {
           $eventInfo[i].textContent = data.currentEvent.artist;
         }
-        if ($eventInfo[i].textContent === 'venuePlaceholder') {
+        if ($eventInfo[i].getAttribute('id') === 'venue') {
           $eventInfo[i].textContent = data.currentEvent.venue;
         }
-        if ($eventInfo[i].textContent === 'addressPlaceholder') {
+        if ($eventInfo[i].getAttribute('id') === 'address') {
           $eventInfo[i].textContent = data.currentEvent.address;
         }
-        if ($eventInfo[i].textContent === 'datePlaceholder') {
+        if ($eventInfo[i].getAttribute('id') === 'date') {
           $eventInfo[i].textContent = data.currentEvent.date;
         }
       } else if ($eventInfo[i].tagName === 'A') {
         $eventInfo[i].textContent = data.currentEvent.name;
         $eventInfo[i].setAttribute('href', data.currentEvent.eventURL);
       }
-      toggleVisible(i);
     }
   });
+  xhr.send();
 }
 $searchForm.addEventListener('submit', searchHandler);
 
 function fullSendHandler(event) {
   for (var i = 0; i < $eventInfo.length; i++) {
+    if ($eventInfo[i].tagName === 'P') {
+      if ($eventInfo[i].textContent === data.currentEvent.artist) {
+        $eventInfo[i].textContent = '';
+      }
+      if ($eventInfo[i].textContent === data.currentEvent.venue) {
+        $eventInfo[i].textContent = '';
+      }
+      if ($eventInfo[i].textContent === data.currentEvent.address) {
+        $eventInfo[i].textContent = '';
+      }
+      if ($eventInfo[i].textContent === data.currentEvent.date) {
+        $eventInfo[i].textContent = '';
+      }
+    }
     toggleVisible(i);
     $previewImage.setAttribute('src', 'https://raw.githubusercontent.com/joshangeles/code-journal/main/images/placeholder-image-square.jpg');
     $searchForm.reset();
